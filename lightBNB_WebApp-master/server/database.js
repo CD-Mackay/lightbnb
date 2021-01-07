83a22622f55a77f3cd73a02f4d5183832c7f6045
CD-Mackay <connor.mackay@gmail.com>
2021-01-07 19:07:48 +0000

    implemented search filter for findAllProperties function

--- lightBNB_WebApp-master/server/database.js
+++ lightBNB_WebApp-master/server/database.js
@@ -98,7 +98,6 @@ exports.getAllReservations = getAllReservations;
  */
 const getAllProperties = function(options, limit = 10) {
   const queryParams = [];
-
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) AS average_rating
   FROM properties
@@ -110,22 +109,37 @@ const getAllProperties = function(options, limit = 10) {
     queryString += `WHERE city LIKE $${queryParams.length} `;
   }
 
-  if (options.minimum_rating) {
-    queryParams.push(`%${options.minimum_rating}%`);
-    queryString += `AND HAVING AVG(property_reviews.rating) >= $${queryParams.length}`
+  if (options.owner_id) {
+   queryParams.push(`${options.owner_id}`)
+   queryString += `AND owner_id = $${queryParams.length} `;
   }
 
-  // if (options.owner_id) {
-  //   queryParams.push(`%${options.owner_id}`);
-  //   queryString += `AND owner_id = $${queryParams.length}`
-  // }
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryString += `AND cost_per_night / 100 >= $${queryParams.length} `;
+  }
 
-  queryParams.push(limit);
-  queryString += `
-  GROUP BY properties.id
-  ORDER BY cost_per_night
-  LIMIT $${queryParams.length};
-  `;
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`);
+    queryString += `AND cost_per_night / 100 <= $${queryParams.length}`
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `GROUP BY properties.id
+                    HAVING AVG(property_reviews.rating) >= $${queryParams.length}
+                    ORDER BY cost_per_night `
+    queryParams.push(limit);
+    queryString += `LIMIT $${queryParams.length};`
+  } else {
+    queryParams.push(limit);
+    queryString += `
+    GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  }
+  
 
   console.log(queryString, queryParams);
 
